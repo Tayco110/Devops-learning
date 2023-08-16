445ede21d0a1f5d383f89f28a2e5469c1f44dc0b
Tayco Rodrigues <tmsr@ic.ufal.br>
2023-08-16 09:46:26 -0300

    task 3
    Signed-off-by: Tayco Rodrigues <tmsr@ic.ufal.br>

--- task-3/app/main.py
+++ task-3/app/main.py
@@ -1,18 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 app = FastAPI()
 
-class SecretValues(BaseModel):
-    DATABASE_URL: str
-    API_KEY: str
-
-@app.get("/secrets", response_model=SecretValues)
-def get_secret_values():
-    database_url = os.getenv("DATABASE_URL")
-    api_key = os.getenv("API_KEY")
-    return SecretValues(DATABASE_URL=database_url, API_KEY=api_key)
+@app.get("/vars")
+async def read_root():
+    environment_vars = os.environ
+    return environment_vars
